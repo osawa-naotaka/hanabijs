160429d2b193a1482ffac5ac04d908ab43f3bdc8
osawa naotaka <ohsawa.naotaka@gmail.com>
2025-03-31 01:58:50 +0900

    add hIcon. refactoring. fix for lint.

--- site/components/element/shareX.ts
+++ site/components/element/shareX.ts
@@ -1,6 +1,7 @@
 import { BOLD, FONT_SIZE, F_SMALL, PADDING, S_SMALL, S_TINY } from "@/lib/stylerules";
 import { hLinkedButton } from "@/lib/ui/button";
-import { I, Span } from "@/main";
+import { hIcone } from "@/lib/ui/icon";
+import { Span } from "@/main";
 import type { HComponentFn, Store } from "@/main";
 
 export type ShareXArgument = {
@@ -16,11 +17,12 @@ export function shareX(store: Store): HComponentFn<ShareXArgument> {
         BOLD,
         PADDING(S_TINY(store), S_SMALL(store)),
     );
+    const XIcon = hIcone({ type: "brands", name: "x-twitter" })({})();
 
     return {
-        [".share-x"]: (argument: ShareXArgument) => () => {
-            const link = `https://x.com/intent/tweet?text=${encodeURIComponent(argument.title)}&url=${encodeURIComponent(argument.url)}`;
-            return ShareX({ href: link })(I({ class: ["fa-brands", "fa-x-twitter"] })(), Span({})("SHARE"));
+        ".share-x": (argument: ShareXArgument) => () => {
+            const href = `https://x.com/intent/tweet?text=${encodeURIComponent(argument.title)}&url=${encodeURIComponent(argument.url)}`;
+            return ShareX({ href })(XIcon, Span({})("SHARE"));
         },
     }[".share-x"];
 }
