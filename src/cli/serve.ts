5e217cf3963c80c62295035108564dfb601738e1
osawa naotaka <ohsawa.naotaka@gmail.com>
2025-04-19 18:23:42 +0900

    add bun specific server.

--- src/cli/serve.ts
+++ src/cli/serve.ts
@@ -21,38 +21,70 @@ import { stringifyToHtml } from "@/lib/server/serverfn";
 import { ErrorPage } from "@/page/error";
 import { hanabi_error_css } from "@/page/hanabi-error";
 import chokidar from "chokidar";
+import type { FSWatcher } from "chokidar";
 import { WebSocketServer } from "ws";
 
 export async function serve(conf_file: string | undefined) {
     const config = await loadConfig(conf_file);
-
-    const root = cwd();
-    const page_dir = path.join(root, config.input.page_dir);
-    const public_dir = path.join(root, config.input.public_dir);
-    const watch_dir = path.join(root, config.server.watch_dir);
-
+    const watch_dir = path.join(cwd(), config.server.watch_dir);
     const watcher = chokidar.watch(watch_dir, { persistent: true });
-    const store = generateStore(config.asset, config.designrule);
 
-    let page_router = await createPageRouter(page_dir);
-    let public_router = await createStaticRouter(public_dir);
-    let asset_router = new Map<string, Router>();
+    const [proc, reload] = await createReqProcessor(config);
 
-    const require = createRequire(import.meta.url);
+    if (Bun !== undefined) {
+        await createAndStartBunSearver(config, proc, reload, watcher);
+    } else {
+        createAndStartNodeServer(config, proc, reload, watcher);
+    }
+}
 
+async function createAndStartBunSearver(
+    config: HanabiConfig,
+    proc: ReqProcessFn,
+    reload: ReloadFn,
+    watcher: FSWatcher,
+): Promise<void> {
+    const server = Bun.serve({
+        websocket: {
+            open(ws) {
+                watcher.removeAllListeners();
+                watcher.on("change", async () => {
+                    await reload();
+                    ws.send("reload");
+                });
+            },
+            message(_ws, message) {
+                console.log("Received:", message);
+            },
+            close() {
+                watcher.removeAllListeners();
+            },
+        },
+        async fetch(req: Request): Promise<Response> {
+            if (server.upgrade(req)) {
+                return new Response(null, { status: 101 });
+            }
+            const rv = await proc(req);
+            return new Response(rv.content, { status: rv.status, headers: { "Content-Type": rv.type } });
+        },
+        port: config.server.port,
+        hostname: config.server.hostname,
+        async error(error) {
+            console.error(error);
+            return new Response(error.message, { status: 500, headers: { "Content-Type": "text/plain" } });
+        },
+    });
+}
+
+function createAndStartNodeServer(
+    config: HanabiConfig,
+    proc: ReqProcessFn,
+    reload: ReloadFn,
+    watcher: FSWatcher,
+): void {
     const http_server = http.createServer(async (msg: IncomingMessage, resp: ServerResponse) => {
         const req: Request = new Request(new URL(`http://${msg.headers.host}${msg.url}`));
-        const rv = await processRequest(
-            req,
-            store,
-            page_router,
-            asset_router,
-            public_router,
-            page_dir,
-            public_dir,
-            config,
-            require,
-        );
+        const rv = await proc(req);
         resp.writeHead(rv.status, { "Content-Type": rv.type });
         resp.end(rv.content);
     });
@@ -73,12 +105,7 @@ export async function serve(conf_file: string | undefined) {
             wss.handleUpgrade(req, socket, head, (ws) => {
                 watcher.on("change", async () => {
                     watcher.removeAllListeners();
-                    for (const key of Object.keys(require.cache)) {
-                        delete require.cache[key];
-                    }
-                    page_router = await createPageRouter(page_dir);
-                    public_router = await createStaticRouter(public_dir);
-                    asset_router = new Map<string, Router>();
+                    await reload();
                     ws.send("reload");
                 });
                 wss.emit("connection", ws, req);
@@ -120,109 +147,127 @@ function errorResponse(status: number, cause: string): Resp {
     };
 }
 
-async function processRequest(
-    req: Request,
-    store: Store,
-    page_router: Router,
-    asset_router: Map<string, Router>,
-    public_router: Router,
-    page_dir: string,
-    public_dir: string,
-    config: HanabiConfig,
-    require: NodeJS.Require,
-): Promise<Resp> {
-    try {
-        // Page router
-        const match_page = page_router(req);
-        if (!(match_page instanceof Error)) {
-            const page_fn = require(path.join(page_dir, match_page.target_file));
-            if (typeof page_fn.default === "function") {
-                clearStore(store);
-                const root_page_fn = await page_fn.default(store);
+type ReqProcessFn = (req: Request) => Promise<Resp>;
+type ReloadFn = () => Promise<void>;
 
-                // auto generation of .css , .js and .woff2 from .html.ts
-                if (match_page.auto_generate) {
-                    switch (match_page.req_ext) {
-                        case ".css": {
-                            const css_name = withoutExt(withoutExt(match_page.target_file));
-                            const css = await bundleCss(store, css_name, require);
-                            if (css instanceof Error) {
-                                return errorResponse(500, css.message);
+async function createReqProcessor(config: HanabiConfig): Promise<[ReqProcessFn, ReloadFn]> {
+    const root = cwd();
+    const page_dir = path.join(root, config.input.page_dir);
+    const public_dir = path.join(root, config.input.public_dir);
+
+    const store = generateStore(config.asset, config.designrule);
+
+    let page_router = await createPageRouter(page_dir);
+    let public_router = await createStaticRouter(public_dir);
+    let asset_router = new Map<string, Router>();
+
+    const require = createRequire(import.meta.url);
+
+    const reload_fn: ReloadFn = async () => {
+        for (const key of Object.keys(require.cache)) {
+            delete require.cache[key];
+        }
+        page_router = await createPageRouter(page_dir);
+        public_router = await createStaticRouter(public_dir);
+        asset_router = new Map<string, Router>();
+    };
+
+    const proc_fn: ReqProcessFn = async (req: Request) => {
+        try {
+            // Page router
+            const match_page = page_router(req);
+            if (!(match_page instanceof Error)) {
+                const page_fn = require(path.join(page_dir, match_page.target_file));
+                if (typeof page_fn.default === "function") {
+                    clearStore(store);
+                    const root_page_fn = await page_fn.default(store);
+
+                    // auto generation of .css , .js and .woff2 from .html.ts
+                    if (match_page.auto_generate) {
+                        switch (match_page.req_ext) {
+                            case ".css": {
+                                const css_name = withoutExt(withoutExt(match_page.target_file));
+                                const css = await bundleCss(store, css_name, require);
+                                if (css instanceof Error) {
+                                    return errorResponse(500, css.message);
+                                }
+                                return normalResponse(css || "", match_page.req_ext);
                             }
-                            return normalResponse(css || "", match_page.req_ext);
+                            case ".js": {
+                                const js = await bundleScriptEsbuild(store);
+                                return normalResponse(js || "", match_page.req_ext);
+                            }
+                            case ".woff2": {
+                                const woff2 = await bundleWoff2(store);
+                                return normalResponse(woff2 || "", match_page.req_ext);
+                            }
+                            default:
+                                return errorResponse(500, `auto generation of ${match_page.req_ext} is not supported.`);
                         }
-                        case ".js": {
-                            const js = await bundleScriptEsbuild(store);
-                            return normalResponse(js || "", match_page.req_ext);
-                        }
-                        case ".woff2": {
-                            const woff2 = await bundleWoff2(store);
-                            return normalResponse(woff2 || "", match_page.req_ext);
+                    }
+
+                    switch (match_page.req_ext) {
+                        case ".html": {
+                            const router_set = await createAssetRouterSet(store, config.asset.target_prefix);
+                            for (const [key, router] of router_set) {
+                                asset_router.set(key, router);
+                            }
+
+                            const css_name = `${withoutExt(withoutExt(match_page.target_file))}.css`;
+                            const js_name = `${withoutExt(withoutExt(match_page.target_file))}.js`;
+                            const insert_nodes = [
+                                Script({ type: "module", src: "/reload.js" }),
+                                Script({ type: "module", src: js_name }),
+                                Link({ href: css_name, rel: "stylesheet" }),
+                            ];
+
+                            const html_text = await bundleHtml(store, match_page.params, root_page_fn, insert_nodes);
+
+                            return normalResponse(html_text, ".html");
                         }
                         default:
-                            return errorResponse(500, `auto generation of ${match_page.req_ext} is not supported.`);
+                            return normalResponse(await root_page_fn, match_page.req_ext);
                     }
                 }
+                return errorResponse(500, `${match_page.target_file} does not have default export.`);
+            }
 
-                switch (match_page.req_ext) {
-                    case ".html": {
-                        const router_set = await createAssetRouterSet(store, config.asset.target_prefix);
-                        for (const [key, router] of router_set) {
-                            asset_router.set(key, router);
-                        }
+            // Public router
+            const match_public = public_router(req);
+            if (!(match_public instanceof Error)) {
+                const content = await readFile(path.join(public_dir, match_public.target_file));
+                return normalResponse(content, match_public.req_ext);
+            }
 
-                        const css_name = `${withoutExt(withoutExt(match_page.target_file))}.css`;
-                        const js_name = `${withoutExt(withoutExt(match_page.target_file))}.js`;
-                        const insert_nodes = [
-                            Script({ type: "module", src: "/reload.js" }),
-                            Script({ type: "module", src: js_name }),
-                            Link({ href: css_name, rel: "stylesheet" }),
-                        ];
-
-                        const html_text = await bundleHtml(store, match_page.params, root_page_fn, insert_nodes);
-
-                        return normalResponse(html_text, ".html");
-                    }
-                    default:
-                        return normalResponse(await root_page_fn, match_page.req_ext);
+            // Asset router
+            for (const router of asset_router.values()) {
+                const match_asset = router(req);
+                if (!(match_asset instanceof Error)) {
+                    const content = await readFile(match_asset.target_file);
+                    return normalResponse(content, match_asset.req_ext);
                 }
             }
-            return errorResponse(500, `${match_page.target_file} does not have default export.`);
-        }
 
-        // Public router
-        const match_public = public_router(req);
-        if (!(match_public instanceof Error)) {
-            const content = await readFile(path.join(public_dir, match_public.target_file));
-            return normalResponse(content, match_public.req_ext);
-        }
-
-        // Asset router
-        for (const router of asset_router.values()) {
-            const match_asset = router(req);
-            if (!(match_asset instanceof Error)) {
-                const content = await readFile(match_asset.target_file);
-                return normalResponse(content, match_asset.req_ext);
+            // reload plugin
+            if (new URL(req.url).pathname.endsWith("/reload.js")) {
+                const reload =
+                    "const ws = new WebSocket(`ws://${location.host}/reload`); ws.onmessage = (event) => { if (event.data === 'reload') { location.reload(); } }";
+                return normalResponse(reload, ".js");
             }
-        }
 
-        // reload plugin
-        if (new URL(req.url).pathname.endsWith("/reload.js")) {
-            const reload =
-                "const ws = new WebSocket(`ws://${location.host}/reload`); ws.onmessage = (event) => { if (event.data === 'reload') { location.reload(); } }";
-            return normalResponse(reload, ".js");
-        }
+            // css for error page
+            if (new URL(req.url).pathname.localeCompare("/hanabi-error.css") === 0) {
+                return normalResponse(hanabi_error_css, ".css");
+            }
 
-        // css for error page
-        if (new URL(req.url).pathname.localeCompare("/hanabi-error.css") === 0) {
-            return normalResponse(hanabi_error_css, ".css");
+            return errorResponse(404, `route for url "${req.url}" not found.`);
+        } catch (e) {
+            if (e instanceof Error) {
+                return errorResponse(500, e.toString());
+            }
+            throw e;
         }
+    };
 
-        return errorResponse(404, `route for url "${req.url}" not found.`);
-    } catch (e) {
-        if (e instanceof Error) {
-            return errorResponse(500, e.toString());
-        }
-        throw e;
-    }
+    return [proc_fn, reload_fn];
 }
